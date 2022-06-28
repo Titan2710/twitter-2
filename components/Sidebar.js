d86aec46306b328f55bd442ec41021725f511bb4
Wai Hlyan <waihlyanwin007@gmail.com>
2022-06-28 20:19:13 +0630

    half way post section

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -16,7 +16,7 @@ import { useSession } from "next-auth/react";
 
 function Sidebar() {
 
-  const {data : session} = useSession();
+  const {data: session} = useSession();
 
   return (
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
